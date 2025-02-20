97a353fa2142e9927a23848fc62069b041561ea4
CrabbyPete <86552+CrabbyPete@users.noreply.github.com>
2025-02-20 16:31:41 -0500

    Initial commit

--- apt_install.sh
+++ apt_install.sh
@@ -5,11 +5,21 @@ apt upgrade -y
 raspi-config nonint do_i2c 1
 raspi-config nonint do_serial 1
 
-apt install -y python3-pip
-apt install -y pigpiod python3-pigpio
-apt install -y redis
-apt install -y i2c-tools
-apt install -y emacs-nox
+apt install -y python3-pip python3-venv
+apt install -y git emacs-nox redis i2c-tools
+apt install -y python3-rpi.gpio python3-arrow python3-redis python3-pymodbus
+apt install -y libgstreamer1.0-dev \
+  gstreamer1.0-alsa \
+  libgstreamer-plugins-base1.0-dev \
+  libgstreamer-plugins-bad1.0-dev \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav \
+  gstreamer1.0-tools \
+  gstreamer1.0-libcamera
 
-systemctl enable pigpiod
-systemctl start pigpiod
+pip3 install --break-system-packages smbus2
+
+# python3-gst-1.0
---
+++ src/play.py
@@ -0,0 +1,7 @@
+
+from sensorctl import speakers
+
+if __name__ == "__main__":
+    while True:
+        speakers('play',[])
+
